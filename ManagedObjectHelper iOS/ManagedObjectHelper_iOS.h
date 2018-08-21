29d4b8b394fb104d003138fbde16b7784a44fa2a
Ryosuke IWANAGA <me@riywo.com>
2018-08-20 22:29:37 -0700

    Add targets

---
+++ ManagedObjectHelper iOS/ManagedObjectHelper_iOS.h
@@ -0,0 +1,19 @@
+//
+//  ManagedObjectHelper_iOS.h
+//  ManagedObjectHelper iOS
+//
+//  Created by Ryosuke Iwanaga on 2018-08-20.
+//  Copyright © 2018 Ryosuke Iwanaga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ManagedObjectHelper_iOS.
+FOUNDATION_EXPORT double ManagedObjectHelper_iOSVersionNumber;
+
+//! Project version string for ManagedObjectHelper_iOS.
+FOUNDATION_EXPORT const unsigned char ManagedObjectHelper_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ManagedObjectHelper_iOS/PublicHeader.h>
+
+
