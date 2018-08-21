7d4a6fe7294bd73b2272a47940087937a66bc226
Ryosuke IWANAGA <me@riywo.com>
2018-08-20 22:28:20 -0700

    Initial Commit

---
+++ ManagedObjectHelper/ManagedObjectHelper.h
@@ -0,0 +1,19 @@
+//
+//  ManagedObjectHelper.h
+//  ManagedObjectHelper
+//
+//  Created by Ryosuke Iwanaga on 2018-08-20.
+//  Copyright © 2018 Ryosuke Iwanaga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ManagedObjectHelper.
+FOUNDATION_EXPORT double ManagedObjectHelperVersionNumber;
+
+//! Project version string for ManagedObjectHelper.
+FOUNDATION_EXPORT const unsigned char ManagedObjectHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ManagedObjectHelper/PublicHeader.h>
+
+
